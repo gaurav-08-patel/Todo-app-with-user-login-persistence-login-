4a1186b31c7881062a8c83fa87ba8426251659d0
gaurav-08-patel <gauravpatel.rounak@gmail.com>
2025-07-12 23:33:57 +0545

    added new  .env variable

--- config/db.js
+++ config/db.js
@@ -3,7 +3,7 @@ import dotenv from "dotenv";
 dotenv.config({ quiet: true });
 
  const dbConfig = {
-    "host" : "localhost",
+    "host" : process.env.DB_HOST,
     "user" : process.env.DB_USER,
     "password" : process.env.DB_PASSWORD,
     "database" : process.env.DB_NAME
